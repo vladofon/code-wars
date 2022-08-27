772f73d2334dca2a3f725163fc3d90e8af7684e1
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-08-27 22:41:27 +0300

    complete "How many numbers 1" task

--- code-wars/src/main/java/com/codewars/tasks/how_many_numbers/HowManyNumbers.java
+++ code-wars/src/main/java/com/codewars/tasks/how_many_numbers/HowManyNumbers.java
@@ -1,8 +1,11 @@
 package com.codewars.tasks.how_many_numbers;
 
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 public class HowManyNumbers {
+
+	// my solution
 	public static int selNumber(int n, int d) {
 		return IntStream.range(10, n + 1).filter(e -> {
 			int item = e / (int) Math.pow(10, String.valueOf(e).length() - 1);
@@ -16,5 +19,13 @@ public class HowManyNumbers {
 			System.out.println(e);
 			return true;
 		}).toArray().length;
+	}
+
+	// best solution
+	public static int selNumberBest(int n, int d) {
+		Predicate<String> seq = s -> IntStream.rangeClosed(0, s.length() - 2).map(i -> s.charAt(i + 1) - s.charAt(i))
+				.allMatch(i -> i > 0 && i <= d);
+
+		return (int) IntStream.rangeClosed(12, n).filter(i -> seq.test(i + "")).count();
 	}
 }
